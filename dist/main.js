1bafc9ddb3ce44b91c593a2537f9c06053e7b101
Will <william.ingram@concentra.co.uk>
2021-12-08 18:47:45 +0000

    example output

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "foo", () => $854d6c54c92032c0$exports);
+var $854d6c54c92032c0$exports = {};
+
+$parcel$export($854d6c54c92032c0$exports, "a", () => $854d6c54c92032c0$export$407448d2b89b1813);
+const $854d6c54c92032c0$export$407448d2b89b1813 = 1;
+
+
+
+
+//# sourceMappingURL=main.js.map
---
+++ dist/module.js
@@ -0,0 +1,12 @@
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+var $5d563c0f6c8d2e9b$exports = {};
+
+$parcel$export($5d563c0f6c8d2e9b$exports, "a", () => $5d563c0f6c8d2e9b$export$407448d2b89b1813);
+const $5d563c0f6c8d2e9b$export$407448d2b89b1813 = 1;
+
+
+
+
+//# sourceMappingURL=module.js.map
