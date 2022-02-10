0d048a36213e1bd5497c809efd3a779673279bdd
Will <william.ingram@concentra.co.uk>
2022-02-10 10:56:07 +0000

    update to Parcel 2.3.0

--- dist/main.js
+++ dist/main.js
@@ -2,11 +2,11 @@ function $parcel$export(e, n, v, s) {
   Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
 }
 
-$parcel$export(module.exports, "foo", () => $854d6c54c92032c0$exports);
-var $854d6c54c92032c0$exports = {};
+$parcel$export(module.exports, "foo", () => $4dd9fb5909bb7c71$exports);
+var $4dd9fb5909bb7c71$exports = {};
 
-$parcel$export($854d6c54c92032c0$exports, "a", () => $854d6c54c92032c0$export$407448d2b89b1813);
-const $854d6c54c92032c0$export$407448d2b89b1813 = 1;
+$parcel$export($4dd9fb5909bb7c71$exports, "a", () => $4dd9fb5909bb7c71$export$407448d2b89b1813);
+const $4dd9fb5909bb7c71$export$407448d2b89b1813 = 1;
 
 
 
--- dist/module.js
+++ dist/module.js
@@ -1,10 +1,10 @@
 function $parcel$export(e, n, v, s) {
   Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
 }
-var $5d563c0f6c8d2e9b$exports = {};
+var $a00fdc490d9c31da$exports = {};
 
-$parcel$export($5d563c0f6c8d2e9b$exports, "a", () => $5d563c0f6c8d2e9b$export$407448d2b89b1813);
-const $5d563c0f6c8d2e9b$export$407448d2b89b1813 = 1;
+$parcel$export($a00fdc490d9c31da$exports, "a", () => $a00fdc490d9c31da$export$407448d2b89b1813);
+const $a00fdc490d9c31da$export$407448d2b89b1813 = 1;
 
 
 
